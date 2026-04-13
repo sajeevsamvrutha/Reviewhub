e18ff06b666ff62b626d46af254a59138f7ad2d0
sajeevsamvrutha <sajeevsamvrutha@gmail.com>
2026-04-13 12:31:17 +0530

    Create books.js

---
+++ books.js
@@ -0,0 +1,8 @@
+const books = [
+  { id: 201, title: "Harry Potter", category: "Books", img: "https://covers.openlibrary.org/b/id/7984916-L.jpg", desc: "Magic", author: "Rowling" },
+  { id: 202, title: "Hobbit", category: "Books", img: "https://covers.openlibrary.org/b/id/6979861-L.jpg", desc: "Fantasy", author: "Tolkien" },
+  { id: 203, title: "1984", category: "Books", img: "https://covers.openlibrary.org/b/id/7222246-L.jpg", desc: "Dystopia", author: "Orwell" },
+  { id: 204, title: "Atomic Habits", category: "Books", img: "https://covers.openlibrary.org/b/id/11153229-L.jpg", desc: "Self help", author: "Clear" },
+  { id: 205, title: "Alchemist", category: "Books", img: "https://covers.openlibrary.org/b/id/8279256-L.jpg", desc: "Philosophy", author: "Coelho" },
+  { id: 206, title: "Rich Dad", category: "Books", img: "https://covers.openlibrary.org/b/id/8091016-L.jpg", desc: "Finance", author: "Kiyosaki" }
+];
