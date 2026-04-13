d1fed69eb509ac1daa30ec25c011e6b9b7e0391b
sajeevsamvrutha <sajeevsamvrutha@gmail.com>
2026-04-13 12:29:46 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,8 @@
+const movies = [
+  { id: 1, title: "Inception", category: "Movies", img: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg", desc: "Mind thriller", director: "Nolan" },
+  { id: 2, title: "Interstellar", category: "Movies", img: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg", desc: "Space story", director: "Nolan" },
+  { id: 3, title: "Joker", category: "Movies", img: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", desc: "Dark story", director: "Todd" },
+  { id: 4, title: "Avatar", category: "Movies", img: "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg", desc: "Alien world", director: "Cameron" },
+  { id: 5, title: "Titanic", category: "Movies", img: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", desc: "Love story", director: "Cameron" },
+  { id: 6, title: "Endgame", category: "Movies", img: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg", desc: "Avengers", director: "Russo" }
+];
